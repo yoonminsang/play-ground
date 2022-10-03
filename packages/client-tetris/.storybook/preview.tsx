709f41def700413db93b34f61baac7ca37315911
yoonminsnag <apdlyooapdl@naver.com>
2022-10-03 23:55:29 +0900

    feat: 스토리북 decorator 설정

--- packages/client-tetris/.storybook/preview.tsx
+++ packages/client-tetris/.storybook/preview.tsx
@@ -1,6 +1,15 @@
+import { GlobalStyle, theme } from '@common/styles';
+import { ThemeProvider } from '@emotion/react';
 import { DecoratorFn } from '@storybook/react';
 
-export const decorators: DecoratorFn[] = [(Story) => <Story />];
+export const decorators: DecoratorFn[] = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
