18c2dda7f30406f1cf2ac70c4c369d9315a61795
yoonminsnag <apdlyooapdl@naver.com>
2022-10-10 18:47:28 +0900

    refactor: TodolistInputs 컴포넌트 분리

--- packages/client-todo-list/src/pages/useSyncExternalStore/index.tsx
+++ packages/client-todo-list/src/pages/useSyncExternalStore/index.tsx
@@ -1,78 +1,19 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
 import { css } from '@emotion/react';
 
-import { useTodoStore } from 'hooks/useTodoStore';
 import { TodoList } from 'components';
-import Folder from 'models/Folder';
-import Task from 'models/Task';
+import TodoListInputs from 'components/TodoListInputs';
 
 interface Props {}
 const UseSyncExternalStorePage: FC<Props> = () => {
-  const [{ selectedFolderIndex }, todoStore] = useTodoStore();
-
-  const [folderValue, setFolderValue] = useState<string>('');
-  const [taskValue, setTaskValue] = useState<string>('');
-
   return (
     <div
-      css={(theme) => css`
+      css={() => css`
         margin: 10px;
-        .inputs-wrapper {
-          display: flex;
-          gap: 10px;
-        }
-        .input-wrapper {
-          display: flex;
-          flex-direction: column;
-          gap: 3px;
-          label {
-            ${theme.typo.labelM}
-          }
-          input {
-            border: 1px solid ${theme.color.grey500};
-            border-radius: 10px;
-            padding: 0 5px;
-            ${theme.typo.bodyM}
-          }
-        }
       `}
     >
-      <div className="inputs-wrapper">
-        <div className="input-wrapper">
-          <label htmlFor="folder">folder</label>
-          <input
-            id="folder"
-            type="text"
-            value={folderValue}
-            onChange={(e) => setFolderValue(e.target.value)}
-            onKeyUp={(e) => {
-              if (e.code === 'Enter') {
-                const folder = new Folder(folderValue);
-                todoStore.addFolder(folder);
-                setFolderValue('');
-              }
-            }}
-          />
-        </div>
-        {selectedFolderIndex !== null && (
-          <div className="input-wrapper">
-            <label htmlFor="task">task</label>
-            <input
-              id="task"
-              type="text"
-              value={taskValue}
-              onChange={(e) => setTaskValue(e.target.value)}
-              onKeyUp={(e) => {
-                if (e.code === 'Enter') {
-                  todoStore.folders[selectedFolderIndex].addTask(new Task({ title: taskValue }));
-                  setTaskValue('');
-                }
-              }}
-            />
-          </div>
-        )}
-      </div>
+      <TodoListInputs />
       <TodoList />
     </div>
   );
