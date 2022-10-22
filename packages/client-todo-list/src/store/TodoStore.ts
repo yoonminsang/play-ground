75d71c7f9046326da7e5fe9658068176c602afa0
yoonminsnag <apdlyooapdl@naver.com>
2022-10-22 18:16:44 +0900

    chore: 주석제거

--- packages/client-todo-list/src/store/TodoStore.ts
+++ packages/client-todo-list/src/store/TodoStore.ts
@@ -5,7 +5,6 @@ import Task from 'models/Task';
 @Store()
 class TodoStore {
   private _folders: Folder[] = [];
-  // TODO: selectedFolderId로 변경할 예정. 그 후에는 selectedFolder로 변경할 예정.
   private _selectedFolderId: null | number = null;
 
   @Action()
@@ -63,7 +62,6 @@ class TodoStore {
     return this._folders.find((v) => v === folder);
   }
 
-  // TODO: 스냅샷이 필요할까? 그냥 getter만 쓰면 어떨까 혹은 멤버변수를 public으로 관리하면 어떨까...
   public get snapshot() {
     return {
       folders: this._folders,
