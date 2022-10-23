c5213c3467aa349e8f3d9727ff582b1b0cc374ea
yoonminsnag <apdlyooapdl@naver.com>
2022-10-23 19:21:30 +0900

    refactor: center 디렉토리 변경

--- packages/common-styles/src/theme/index.ts
+++ packages/common-styles/src/theme/index.ts
@@ -2,7 +2,7 @@ import { color } from './color';
 import { media } from './media';
 import { typo } from './typo';
 import { scroll } from './scroll';
-import { center } from './center';
+import { center } from './utils/center';
 import { lineClamp, truncate } from './utils/ellipsis';
 
 export const theme = {
