707000767cfab649878f6ed4a26d4b6e1515551f
yoonminsnag <apdlyooapdl@naver.com>
2022-10-23 18:31:13 +0900

    feat: center 스타일 추가

--- packages/common-styles/src/theme/helper.style.ts
+++ packages/common-styles/src/theme/helper.style.ts
@@ -15,7 +15,20 @@ const lineClamp = (v: number) => css`
   -webkit-line-clamp: ${v};
 `;
 
+const center = css`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+`;
+
 export const helperStyle = {
   truncate,
   lineClamp,
+  center,
 };
