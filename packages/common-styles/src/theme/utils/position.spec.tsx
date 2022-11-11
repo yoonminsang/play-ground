a1f0cd462cf2351f624b69b2f3129e78cb0578e4
yoonminsnag <apdlyooapdl@naver.com>
2022-11-11 23:53:51 +0900

    feat: position 유틸함수에 메서드 추가, 일부타입제거

--- packages/common-styles/src/theme/utils/position.spec.tsx
+++ packages/common-styles/src/theme/utils/position.spec.tsx
@@ -7,6 +7,8 @@ import { position } from './position';
 
 expect.extend(matchers);
 
+const context = describe;
+
 describe('position 테스트', () => {
   it("position('absolute', 0, 0, 0, 0)", () => {
     const { getByTestId } = render(<div data-testid="test" css={position('absolute', 0, 0, 0, 0)} />);
@@ -14,20 +16,6 @@ describe('position 테스트', () => {
     const el = getByTestId('test');
 
     expect(el).toHaveStyleRule('position', 'absolute');
-    expect(el).toHaveStyleRule('top', '0');
-    expect(el).toHaveStyleRule('right', '0');
-    expect(el).toHaveStyleRule('bottom', '0');
-    expect(el).toHaveStyleRule('left', '0');
-  });
-
-  it('position(0, 0, 0, 0)', () => {
-    const { getByTestId } = render(<div data-testid="test" css={position(0, 0, 0, 0)} />);
-
-    const el = getByTestId('test');
-
-    expect(el).toBeInTheDocument();
-
-    expect(el).not.toHaveStyleRule('position', 'absolute');
     expect(el).toHaveStyleRule('top', '0');
     expect(el).toHaveStyleRule('right', '0');
     expect(el).toHaveStyleRule('bottom', '0');
@@ -48,16 +36,95 @@ describe('position 테스트', () => {
     expect(el).toHaveStyleRule('left', '0');
   });
 
-  it('position({top: 0, left: 0})', () => {
-    const { getByTestId } = render(<div data-testid="test" css={position({ top: 0, left: 0 })} />);
+  context('position 메서드', () => {
+    context('absolute', () => {
+      it('position.absolute({top: 0, left: 0})', () => {
+        const { getByTestId } = render(<div data-testid="test" css={position.absolute({ top: 0, left: 0 })} />);
 
-    const el = getByTestId('test');
+        const el = getByTestId('test');
 
-    expect(el).toBeInTheDocument();
+        expect(el).toBeInTheDocument();
 
-    expect(el).toHaveStyleRule('top', '0');
-    expect(el).not.toHaveStyleRule('bottom', '0');
-    expect(el).not.toHaveStyleRule('right', '0');
-    expect(el).toHaveStyleRule('left', '0');
+        expect(el).toHaveStyleRule('position', 'absolute');
+        expect(el).toHaveStyleRule('top', '0');
+        expect(el).not.toHaveStyleRule('bottom', '0');
+        expect(el).not.toHaveStyleRule('right', '0');
+        expect(el).toHaveStyleRule('left', '0');
+      });
+
+      it('position.absolute(0, 0, 0, 0)', () => {
+        const { getByTestId } = render(<div data-testid="test" css={position.absolute(0, 0, 0, 0)} />);
+
+        const el = getByTestId('test');
+
+        expect(el).toBeInTheDocument();
+
+        expect(el).toHaveStyleRule('position', 'absolute');
+        expect(el).toHaveStyleRule('top', '0');
+        expect(el).toHaveStyleRule('bottom', '0');
+        expect(el).toHaveStyleRule('right', '0');
+        expect(el).toHaveStyleRule('left', '0');
+      });
+    });
+
+    context('fixed', () => {
+      it('position.fixed({top: 0, left: 0})', () => {
+        const { getByTestId } = render(<div data-testid="test" css={position.fixed({ top: 0, left: 0 })} />);
+
+        const el = getByTestId('test');
+
+        expect(el).toBeInTheDocument();
+
+        expect(el).toHaveStyleRule('position', 'fixed');
+        expect(el).toHaveStyleRule('top', '0');
+        expect(el).not.toHaveStyleRule('bottom', '0');
+        expect(el).not.toHaveStyleRule('right', '0');
+        expect(el).toHaveStyleRule('left', '0');
+      });
+
+      it('position.fixed(0, 0, 0, 0)', () => {
+        const { getByTestId } = render(<div data-testid="test" css={position.fixed(0, 0, 0, 0)} />);
+
+        const el = getByTestId('test');
+
+        expect(el).toBeInTheDocument();
+
+        expect(el).toHaveStyleRule('position', 'fixed');
+        expect(el).toHaveStyleRule('top', '0');
+        expect(el).toHaveStyleRule('bottom', '0');
+        expect(el).toHaveStyleRule('right', '0');
+        expect(el).toHaveStyleRule('left', '0');
+      });
+    });
+
+    context('sticky', () => {
+      it('position.sticky({top: 0, left: 0})', () => {
+        const { getByTestId } = render(<div data-testid="test" css={position.sticky({ top: 0, left: 0 })} />);
+
+        const el = getByTestId('test');
+
+        expect(el).toBeInTheDocument();
+
+        expect(el).toHaveStyleRule('position', 'sticky');
+        expect(el).toHaveStyleRule('top', '0');
+        expect(el).not.toHaveStyleRule('bottom', '0');
+        expect(el).not.toHaveStyleRule('right', '0');
+        expect(el).toHaveStyleRule('left', '0');
+      });
+
+      it('position.sticky(0, 0, 0, 0)', () => {
+        const { getByTestId } = render(<div data-testid="test" css={position.sticky(0, 0, 0, 0)} />);
+
+        const el = getByTestId('test');
+
+        expect(el).toBeInTheDocument();
+
+        expect(el).toHaveStyleRule('position', 'sticky');
+        expect(el).toHaveStyleRule('top', '0');
+        expect(el).toHaveStyleRule('bottom', '0');
+        expect(el).toHaveStyleRule('right', '0');
+        expect(el).toHaveStyleRule('left', '0');
+      });
+    });
   });
 });
--- packages/common-styles/src/theme/utils/position.ts
+++ packages/common-styles/src/theme/utils/position.ts
@@ -30,13 +30,6 @@ type TopOrRightOrCoordinates = CSSPixelValue | Coordinates;
  *   bottom: CSSPixelValue,
  *   left: CSSPixelValue
  * ): SerializedStyles;
- *
- * function position(
- *   top: CSSPixelValue,
- *   right: CSSPixelValue,
- *   bottom: CSSPixelValue,
- *   left: CSSPixelValue
- * ): SerializedStyles;
  * ```
  *
  * @example
@@ -52,11 +45,12 @@ type TopOrRightOrCoordinates = CSSPixelValue | Coordinates;
  *   left: 0;
  * `;
  *
- * // 첫번째 인자를 생략하고 `top` 부터 넘길 수 있습니다.
- * const allZero = position(0, 0, 0, 0);
- *
  * // 다음처럼도 사용 가능합니다
  * position('absolute', {top: 0, left: 0});
+ *
+ * // 다음처럼도 사용 가능합니다(absolute, fixed, sticky)
+ * position.absolute(0, 0, 0, ,0);
+ * position.absolute({top: 0, left: 0});
  */
 export function position(
   position: Property.Position,
@@ -65,14 +59,7 @@ export function position(
   bottom: CSSPixelValue,
   left: CSSPixelValue,
 ): SerializedStyles;
-export function position(
-  top: CSSPixelValue,
-  right: CSSPixelValue,
-  bottom: CSSPixelValue,
-  left: CSSPixelValue,
-): SerializedStyles;
 export function position(position: Property.Position, coordinates?: Coordinates): SerializedStyles;
-export function position(coordinates: Coordinates): SerializedStyles;
 
 export function position(
   positionOrTopOrCoordinates: PositionOrTopOrCordinates,
@@ -80,19 +67,6 @@ export function position(
   ...values: CSSPixelValue[]
 ) {
   const [top, right, bottom, left] = (() => {
-    // position(coordinates)
-    if (typeof positionOrTopOrCoordinates === 'object') {
-      return [
-        positionOrTopOrCoordinates.top,
-        positionOrTopOrCoordinates.right,
-        positionOrTopOrCoordinates.bottom,
-        positionOrTopOrCoordinates.left,
-      ];
-    }
-    // position(top, right, bottom, left);
-    if (!isPositionValue(positionOrTopOrCoordinates)) {
-      return [positionOrTopOrCoordinates, topOrRightOrCoordinates as CSSPixelValue, ...values];
-    }
     // position(position, coordinates);
     if (!isCSSPixelValue(topOrRightOrCoordinates)) {
       return [
@@ -112,11 +86,30 @@ export function position(
   ]);
 }
 
-function isPositionValue(value: PositionOrTopOrCordinates): value is Property.Position {
+function isPositionValue(value: unknown): value is Property.Position {
   const positions: Property.Position[] = ['static', 'relative', 'absolute', 'fixed', 'sticky', '-webkit-sticky'];
   return positions.includes(value as any);
 }
 
-function isCSSPixelValue(value: TopOrRightOrCoordinates): value is CSSPixelValue {
+function isCSSPixelValue(value: unknown): value is CSSPixelValue {
   return typeof value === 'string' || typeof value === 'number';
+}
+
+position.absolute = createPosition('absolute');
+position.fixed = createPosition('fixed');
+position.sticky = createPosition('sticky');
+
+function createPosition(pos: Property.Position) {
+  function func(coordinates: Coordinates): SerializedStyles;
+  function func(top: CSSPixelValue, right: CSSPixelValue, bottom: CSSPixelValue, left: CSSPixelValue): SerializedStyles;
+  function func(topOrCoordinates: Coordinates | CSSPixelValue, ...values: CSSPixelValue[]) {
+    // position(position, coordinates);
+    if (!isCSSPixelValue(topOrCoordinates)) {
+      return position(pos, topOrCoordinates);
+    }
+    // position(position, top, right, bottom, left);
+    return position(pos, topOrCoordinates, values[0], values[1], values[2]);
+  }
+
+  return func;
 }
